577c0807118722cbc66102b02b60ceaa993eef79
Jay Chesterman <jay.chesterman@live.ca>
2022-03-08 23:32:40 -0500

    fix analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,12 +20,19 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-2C6WV9FQXM",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-2C6WV9FQXM'
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
       },
     },
   ]
